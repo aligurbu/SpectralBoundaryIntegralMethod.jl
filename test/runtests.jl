a2566b0faf0e2fe03e26be47e3d5f22258de5b17
aligurbu <aligurbu@buffalo.edu>
2022-10-19 21:39:29 -0700

    Set up the test folder

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,9 @@
-using SpectralBoundaryIntegralMethod
-using Test
+using Test, SafeTestsets
 
 @testset "SpectralBoundaryIntegralMethod.jl" begin
     # Write your tests here.
+end
+
+@safetestset "This is only a test" begin
+    include("ThisIsOnlyATest.jl")
 end
