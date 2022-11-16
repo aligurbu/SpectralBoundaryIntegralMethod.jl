3c1327db9aec8b472e7794f62dfc79ed739fb52f
aligurbu <aligurbu@buffalo.edu>
2022-11-16 10:00:56 -0800

    Create geometries.jl

---
+++ src/geometries.jl
@@ -0,0 +1,28 @@
+
+"""
+Construct a sphere geometry on the given (thet, phi) grid and Radius
+
+    Position    = Position of center of mass
+    Orientation = Orientation around 'OrientVec'
+"""
+function getSphereGeometry(thet, phi, Radius;
+                           Position = [0; 0; 0],
+                           OrientVec = [0; 0; 0])
+
+    nlat = length(thet)
+    nlon = length(phi)
+    Xi = zeros(nlat, nlon, 3)
+    Xi[:,:,1] = Radius*sin.(thet).*cos.(phi)
+    Xi[:,:,2] = Radius*sin.(thet).*sin.(phi)
+    Xi[:,:,3] = Radius*cos.(thet).*ones(1,nlon)
+
+    Orientation = exp(hat(OrientVec*pi/2))
+    for m = 1:nlat
+        for n = 1:nlon
+            x = Position + Orientation*Xi[m,n,:]
+            Xi[m,n,:] = x
+        end
+    end
+    return Xi
+end
+
