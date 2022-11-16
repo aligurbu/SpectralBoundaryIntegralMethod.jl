4fd88d13461ee4515e44ab1695c04951fc2fa29d
aligurbu <aligurbu@buffalo.edu>
2022-11-16 10:42:55 -0800

    Delete ThisIsOnlyATest.jl

--- test/ThisIsOnlyATest.jl
+++
@@ -1,7 +0,0 @@
-using DrWatson
-quickactivate(@__DIR__, "SpectralBoundaryIntegralMethod")
-using SpectralBoundaryIntegralMethod
-
-@testset "This is only a test" begin
-    @test addme(2,3) ≈ 5
-end
