78c7fb4c500cd4b5bbf5afdd6a5d45b5ad6af24c
aligurbu <aligurbu@buffalo.edu>
2022-11-16 10:01:14 -0800

    Update utilities.jl
    Associated Legendre function

--- src/utilities.jl
+++ src/utilities.jl
@@ -20,3 +20,28 @@ function gridOnSphere(N)
     phi = transpose([(0:1/nlon:(1-1/nlon))*2π;])
     return nlat, nlon, thet, phi, weight
 end
+
+"""
+    Associated Legendre function
+    Pnm[n, m](θ)
+"""
+function associatedLegendreFun(n::Int64, θ::Float64)
+    CondonShortleyPhase = (-1).^[0:n;]'
+    legendre(LegendreOrthoNorm(), 0:n, 0:n, cos(θ)).*CondonShortleyPhase
+end
+"""
+    Associated Legendre function
+    Pnm[n, m](π)
+"""
+function associatedLegendreFun(n::Int64, θ::Irrational{:π})
+    θ = convert(Float64,θ)
+    associatedLegendreFun(n, θ)
+end
+"""
+    Associated Legendre function
+    Pnm[m][θ, n]
+"""
+function associatedLegendreFun(n::Int64, θ::Vector{Float64})
+    Pnm = legendre(LegendreOrthoNorm(), 0:n, 0:n, cos.(θ))
+    [Pnm[:,:,m+1].*(-1)^m for m in 0:n]
+end
