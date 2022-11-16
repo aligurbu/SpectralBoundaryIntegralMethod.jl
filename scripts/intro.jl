8049be3b6025f4dd420822df718ef0999b99ecaa
aligurbu <aligurbu@buffalo.edu>
2022-11-16 15:09:06 -0800

    Spherical harmonic basis functions and weighted basis functions

--- scripts/intro.jl
+++ scripts/intro.jl
@@ -2,4 +2,3 @@ using DrWatson
 quickactivate(@__DIR__, "SpectralBoundaryIntegralMethod")
 using SpectralBoundaryIntegralMethod
 
-addme(42,24)
--- src/utilities.jl
+++ src/utilities.jl
@@ -45,3 +45,22 @@ function associatedLegendreFun(n::Int64, θ::Vector{Float64})
     Pnm = legendre(LegendreOrthoNorm(), 0:n, 0:n, cos.(θ))
     [Pnm[:,:,m+1].*(-1)^m for m in 0:n]
 end
+
+"""
+    Spherical harmonic basis functions and weighted basis functions
+"""
+function sphericalHarmonicBasisFun(nlat, nlon, thet, phi, weight)
+    # Trigonometric functions
+    cos_m_phi = [cos.((m-1)*phi) for m=2:nlat]
+    sin_m_phi = [sin.((m-1)*phi) for m=2:nlat]
+
+    Pnm = associatedLegendreFun(N, thet)
+    Pn0 = Pnm[1]
+    Pn0_wg = Pn0.*weight
+    Pnm_cos_m_phi = [Pnm[m][:,n].*cos_m_phi[m-1] for m=2:N+1 for n=m:N+1]
+    Pnm_sin_m_phi = [Pnm[m][:,n].*sin_m_phi[m-1] for m=2:N+1 for n=m:N+1]
+    Pnm_cos_m_phi_wg = [Pnm_cos_m_phi[k].*weight for k = 1:Int(N*(N+1)/2)]
+    Pnm_sin_m_phi_wg = [Pnm_sin_m_phi[k].*weight for k = 1:Int(N*(N+1)/2)]
+    return Pnm, Pn0, Pnm_cos_m_phi, Pnm_sin_m_phi,
+                Pn0_wg, Pnm_cos_m_phi_wg, Pnm_sin_m_phi_wg
+end
