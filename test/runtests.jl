056037ed2af3d4bbf0bc57830eb7624bb3c1331d
aligurbu <aligurbu@buffalo.edu>
2022-11-16 10:43:17 -0800

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,9 +1,16 @@
+using DrWatson
+quickactivate(@__DIR__, "SpectralBoundaryIntegralMethod")
+using SpectralBoundaryIntegralMethod
 using Test, SafeTestsets
 
 @testset "SpectralBoundaryIntegralMethod.jl" begin
-    # Write your tests here.
+    @test 2+3 == 5
 end
 
-@safetestset "This is only a test" begin
-    include("ThisIsOnlyATest.jl")
+@safetestset "Testing the Gauss-Legendre quadrature" begin
+    include("FastGaussQuadrature.jl")
+end
+
+@safetestset "Testing the associated Legendre functions" begin
+    include("AssociatedLegendrePolynomials.jl")
 end
