92510324d164d710cc91a81d781fbb180e5719b2
aligurbu <aligurbu@buffalo.edu>
2022-11-16 10:01:20 -0800

    Update SpectralBoundaryIntegralMethod.jl

--- src/SpectralBoundaryIntegralMethod.jl
+++ src/SpectralBoundaryIntegralMethod.jl
@@ -1,7 +1,18 @@
 module SpectralBoundaryIntegralMethod
+using DrWatson
 using Revise
+using LinearAlgebra, FastGaussQuadrature
+using AssociatedLegendrePolynomials
+using GLMakie
+GLMakie.activate!()
 
-addme(x,y) = x+y
-export addme
+include(srcdir("utilities.jl"))
+include(srcdir("geometries.jl"))
+include(srcdir("visualize.jl"))
+
+export getSphereGeometry        # see srcdir("geometries.jl")
+export hat, gridOnSphere        # see srcdir("utilities.jl")
+export associatedLegendreFun    # see srcdir("utilities.jl")
+export visualizeGeometry        # see srcdir("visualize.jl")
 
 end
