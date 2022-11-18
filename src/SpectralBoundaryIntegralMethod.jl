a309e9edfdf2c028c637ea23b5fb0be85a652009
aligurbu <aligurbu@buffalo.edu>
2022-11-18 15:41:48 -0800

    derivativesUnitNormalVector

--- src/SpectralBoundaryIntegralMethod.jl
+++ src/SpectralBoundaryIntegralMethod.jl
@@ -12,7 +12,7 @@ include(srcdir("geometries.jl"))
 include(srcdir("visualize.jl"))
 
 ## see srcdir("thinShellMechanics.jl")
-export firstFundamentalFormCoeff, unitNormalVector
+export firstFundamentalFormCoeff, unitNormalVector, derivativesUnitNormalVector
 # see srcdir("geometries.jl")
 export getSphereGeometry
 ## see srcdir("utilities.jl")
--- src/thinShellMechanics.jl
+++ src/thinShellMechanics.jl
@@ -25,3 +25,90 @@ function unitNormalVector(grad_thet_G, grad_phi_G, JGbrev)
                            grad_thet_G[:,:,2].*grad_phi_G[:,:,1])./JGbrev
     return unitNormal
 end
+
+function derivativesUnitNormalVector(unitNormalG,
+                                     grad_thet_G, grad_phi_G,
+                                     grad_thet_thet_G, grad_phi_phi_G,
+                                     grad_thet_phi_G,
+                                     JGbrev)
+    PP = zeros(size(unitNormalG, 1), size(unitNormalG, 2), 6)
+    PP[:,:,1] = 1 .- unitNormalG[:,:,1].*unitNormalG[:,:,1]
+    PP[:,:,2] = 1 .- unitNormalG[:,:,2].*unitNormalG[:,:,2]
+    PP[:,:,3] = 1 .- unitNormalG[:,:,3].*unitNormalG[:,:,3]
+    PP[:,:,4] = - unitNormalG[:,:,2].*unitNormalG[:,:,3]
+    PP[:,:,5] = - unitNormalG[:,:,1].*unitNormalG[:,:,3]
+    PP[:,:,6] = - unitNormalG[:,:,1].*unitNormalG[:,:,2]
+
+    grad_thet_thet_G_X_grad_phi_G = zeros(size(unitNormalG))
+    grad_thet_thet_G_X_grad_phi_G[:,:,1] =
+                            (grad_thet_thet_G[:,:,2].*grad_phi_G[:,:,3] -
+                             grad_thet_thet_G[:,:,3].*grad_phi_G[:,:,2])
+    grad_thet_thet_G_X_grad_phi_G[:,:,2] =
+                            (grad_thet_thet_G[:,:,3].*grad_phi_G[:,:,1] -
+                             grad_thet_thet_G[:,:,1].*grad_phi_G[:,:,3])
+    grad_thet_thet_G_X_grad_phi_G[:,:,3] =
+                            (grad_thet_thet_G[:,:,1].*grad_phi_G[:,:,2] -
+                             grad_thet_thet_G[:,:,2].*grad_phi_G[:,:,1])
+
+    grad_thet_G_X_grad_thet_phi_G = zeros(size(unitNormalG))
+    grad_thet_G_X_grad_thet_phi_G[:,:,1] =
+                            (grad_thet_G[:,:,2].*grad_thet_phi_G[:,:,3] -
+                             grad_thet_G[:,:,3].*grad_thet_phi_G[:,:,2])
+    grad_thet_G_X_grad_thet_phi_G[:,:,2] =
+                            (grad_thet_G[:,:,3].*grad_thet_phi_G[:,:,1] -
+                             grad_thet_G[:,:,1].*grad_thet_phi_G[:,:,3])
+    grad_thet_G_X_grad_thet_phi_G[:,:,3] =
+                            (grad_thet_G[:,:,1].*grad_thet_phi_G[:,:,2] -
+                             grad_thet_G[:,:,2].*grad_thet_phi_G[:,:,1])
+
+    grad_thet_phi_G_X_grad_phi_G = zeros(size(unitNormalG))
+    grad_thet_phi_G_X_grad_phi_G[:,:,1] =
+                            (grad_thet_phi_G[:,:,2].*grad_phi_G[:,:,3] -
+                             grad_thet_phi_G[:,:,3].*grad_phi_G[:,:,2])
+    grad_thet_phi_G_X_grad_phi_G[:,:,2] =
+                            (grad_thet_phi_G[:,:,3].*grad_phi_G[:,:,1] -
+                             grad_thet_phi_G[:,:,1].*grad_phi_G[:,:,3])
+    grad_thet_phi_G_X_grad_phi_G[:,:,3] =
+                            (grad_thet_phi_G[:,:,1].*grad_phi_G[:,:,2] -
+                             grad_thet_phi_G[:,:,2].*grad_phi_G[:,:,1])
+
+    grad_thet_G_X_grad_phi_phi_G = zeros(size(unitNormalG))
+    grad_thet_G_X_grad_phi_phi_G[:,:,1] =
+                            (grad_thet_G[:,:,2].*grad_phi_phi_G[:,:,3] -
+                             grad_thet_G[:,:,3].*grad_phi_phi_G[:,:,2])
+    grad_thet_G_X_grad_phi_phi_G[:,:,2] =
+                            (grad_thet_G[:,:,3].*grad_phi_phi_G[:,:,1] -
+                             grad_thet_G[:,:,1].*grad_phi_phi_G[:,:,3])
+    grad_thet_G_X_grad_phi_phi_G[:,:,3] =
+                            (grad_thet_G[:,:,1].*grad_phi_phi_G[:,:,2] -
+                             grad_thet_G[:,:,2].*grad_phi_phi_G[:,:,1])
+
+    DiffUnitNormalG_thet = (grad_thet_thet_G_X_grad_phi_G +
+                            grad_thet_G_X_grad_thet_phi_G)./JGbrev
+
+    grad_thet_UnitNormalG = zeros(size(unitNormalG))
+    grad_thet_UnitNormalG[:,:,1] = PP[:,:,1].*DiffUnitNormalG_thet[:,:,1] +
+                                    PP[:,:,6].*DiffUnitNormalG_thet[:,:,2] +
+                                    PP[:,:,5].*DiffUnitNormalG_thet[:,:,3]
+    grad_thet_UnitNormalG[:,:,2] = PP[:,:,6].*DiffUnitNormalG_thet[:,:,1] +
+                                    PP[:,:,2].*DiffUnitNormalG_thet[:,:,2] +
+                                    PP[:,:,4].*DiffUnitNormalG_thet[:,:,3]
+    grad_thet_UnitNormalG[:,:,3] = PP[:,:,5].*DiffUnitNormalG_thet[:,:,1] +
+                                    PP[:,:,4].*DiffUnitNormalG_thet[:,:,2] +
+                                    PP[:,:,3].*DiffUnitNormalG_thet[:,:,3]
+
+    DiffUnitNormalG_phi = (grad_thet_phi_G_X_grad_phi_G +
+                            grad_thet_G_X_grad_phi_phi_G)./JGbrev
+
+    grad_phi_UnitNormalG = zeros(size(unitNormalG))
+    grad_phi_UnitNormalG[:,:,1] = PP[:,:,1].*DiffUnitNormalG_phi[:,:,1] +
+                                PP[:,:,6].*DiffUnitNormalG_phi[:,:,2] +
+                                PP[:,:,5].*DiffUnitNormalG_phi[:,:,3]
+    grad_phi_UnitNormalG[:,:,2] = PP[:,:,6].*DiffUnitNormalG_phi[:,:,1] +
+                                PP[:,:,2].*DiffUnitNormalG_phi[:,:,2] +
+                                PP[:,:,4].*DiffUnitNormalG_phi[:,:,3]
+    grad_phi_UnitNormalG[:,:,3] = PP[:,:,5].*DiffUnitNormalG_phi[:,:,1] +
+                                PP[:,:,4].*DiffUnitNormalG_phi[:,:,2] +
+                                PP[:,:,3].*DiffUnitNormalG_phi[:,:,3]
+    return grad_thet_UnitNormalG, grad_phi_UnitNormalG
+end
