bf16d5325ffb117a5ae48b128a48fcf342c0be18
aligurbu <aligurbu@buffalo.edu>
2022-11-18 17:13:46 -0800

    Update visualize.jl

--- src/visualize.jl
+++ src/visualize.jl
@@ -1,6 +1,6 @@
 
 
-function visualizeGeometry(Xi)
+function visualizeGeometry(Xi; Color=:red)
     fig = Figure(resolution = (1200, 800), fontsize = 22)
     ax = Axis3(fig[1, 1], aspect = :data,
             #xgridvisible = false, ygridvisible = false, zgridvisible = false,
@@ -10,13 +10,13 @@ function visualizeGeometry(Xi)
             xticksvisible = false, yticksvisible = false, zticksvisible = false
             )
     sm = surface!(ax, Xi[:,:,1], Xi[:,:,2], Xi[:,:,3];
-                colormap = [:white, :white], transparency = true,
-                lightposition = Vec3f(0, 0, 0.8), ambient = Vec3f(0.6, 0.6, 0.6),
-                backlight = 1.0f0)
+                  colormap = [Color, Color], transparency = true,
+                  lightposition = Vec3f(0, 0, 0.8), ambient = Vec3f(0.6, 0.6, 0.6),
+                  backlight = 1.0f0)
     wireframe!(ax, Xi[:,:,1], Xi[:,:,2], Xi[:,:,3];
-            overdraw = true, transparency = true, color = (:black, 0.7))
+               overdraw = true, transparency = true, color = (:black, 0.7))
     scatter!(ax, Xi[:,:,1][:], Xi[:,:,2][:], Xi[:,:,3][:];
-            color = :black, transparency = true, overdraw = true)
+             color = :black, transparency = true, overdraw = true)
     colsize!(fig.layout, 1, Aspect(1, 1.0))
     fig
 end
